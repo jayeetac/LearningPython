048c9f107f6c889f6085669536f1cbbd1b70acbc
Jayeeta Chakraborty <chakraborty.jayeeta@live.com>
2019-05-08 16:52:50 -0400

    Cleaned up the code.

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,17 +1,12 @@
-def fibonacci():
-    num = int(input("How many numbers that generates?:"))
-    i = 2
-    if num == 0:
-        fib = []
-    elif num == 1:
-        fib = [0]
-    elif num == 2:
-        fib = [0,1]
-    elif num > 2:
-        fib = [0,1]
-        while i < num:
-            fib.append(fib[i-1] + fib[i-2])
-            i += 1
-    return fib
+num = int(input("How many numbers that generates?:"));
+def fibMine (num):
+    fibo = []
+    a =0
+    b =1
+    for i in range (num):
+        fibo.append(a)
+        a,b = b, a+b
 
-print(fibonacci())
+    return fibo
+
+print(fibMine(num))
